13e074c8ad230a51db9e811f2546290dae7fa1d6
James Park <pskpassion@gmail.com>
2021-11-29 03:24:48 +0900

    sss

--- Calendar/urls.py
+++ Calendar/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schedule/', include('schedule.urls')),
+    path('user/', include('user.urls')),
 ]
 
