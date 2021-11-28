b3cbeceb22290a33086204777486c84b63ce7eb8
jerrypark04@gmail.com <jerrypark04@gmail.com>
2021-11-29 03:11:20 +0900

    User first commit

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('user/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
