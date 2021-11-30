90ac3ec83601c0aaaf4eeb1ef701a429a3d54cc6
James Park <pskpassion@gmail.com>
2021-12-01 03:43:37 +0900

    코드에 대한 주석 작성

--- schedule/models.py
+++ schedule/models.py
@@ -26,11 +26,11 @@ from django.utils import timezone
 # >>> Question.objects.get(id=1)
 
 class Calendar(models.Model):
-    userId = models.IntegerField(default=0)
-    title = models.CharField(max_length=100)
-    start = models.DateTimeField('Start Date')
-    end = models.DateTimeField('End Date')
-    allDay = models.BooleanField(default=True)
+    userId = models.IntegerField(default=0) # 사용자 아이디 (Admin users 테이블의 PK)
+    title = models.CharField(max_length=100) # 이벤트 제목
+    start = models.DateTimeField('Start Date') # 이벤트 시작일시
+    end = models.DateTimeField('End Date') # 이벤트 종료일시
+    allDay = models.BooleanField(default=True) # 이벤트가 종일 지속되는지 여부
 
     def __str__(self) -> str:
         return self.title
